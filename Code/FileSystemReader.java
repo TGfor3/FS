fba1afec81c0af2c8540fc759fa0ba59f7554131
Tuvia Goldstein <tjgoldst@mail.yu.edu>
2022-04-27 16:43:06 -0400

    First commit

---
+++ Code/FileSystemReader.java
@@ -0,0 +1,29 @@
+package fs.code;
+import java.util.Scanner;
+
+public class FileSystemReader{
+    public static void main(String[] args) {
+        Scanner cmdScanner = new Scanner(System.in);
+    }
+    public void stop(){
+
+    }
+    public void info(){
+
+    }
+    public void ls(){
+
+    }
+    public void stat(){
+
+    }
+    public void size(){
+
+    }
+    public void cd(){
+
+    }
+    public void read(){
+
+    }
+}
